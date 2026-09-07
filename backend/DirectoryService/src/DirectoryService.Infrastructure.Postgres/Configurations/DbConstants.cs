21861ada610f29c9b6b4dc97e312226a147fff52
Damir <damirzasimenko@gmail.com>
2026-09-07 21:59:41 +0300

    Increase location address DB column length to 500

--- backend/DirectoryService/src/DirectoryService.Infrastructure.Postgres/Configurations/DbConstants.cs
+++ backend/DirectoryService/src/DirectoryService.Infrastructure.Postgres/Configurations/DbConstants.cs
@@ -3,4 +3,5 @@ namespace DirectoryService.Infrastructure.Postgres.Configurations;
 public static class DbConstants
 {
     public const int MaxStringLength = 300;
+    public const int MaxAddressLength = 500;
 }
